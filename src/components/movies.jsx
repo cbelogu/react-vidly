400e591845dfb94bdb09f641d0fb1e6d8547037d
loganathan.rathinasw <Logan.rathinaswamy@tattsgroup.com>
2019-01-16 14:47:14 +1000

    like added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from './common/like';
 
 class Movies extends Component {
     state = {
@@ -8,6 +9,14 @@ class Movies extends Component {
 
     handleDelete = (movie) => {
         const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
+    }
+
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]};
+        movies[index].liked = !movies[index].liked;
         this.setState({movies});
     }
 
@@ -30,6 +39,7 @@ class Movies extends Component {
                     <th>Number In Stock</th>
                     <th>Daily Rental Rate</th>
                     <th></th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -39,6 +49,7 @@ class Movies extends Component {
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
+                    <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)}/></td>
                     <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                 </tr>)}
             </tbody>
